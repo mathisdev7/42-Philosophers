114afcc65520d51ded3c128185d4e01cb7b9bb27
Frost <999frostz@gmail.com>
2025-01-26 19:53:48 +0100

    feat(die, forks): die and forks system done

--- src/main.c
+++ src/main.c
@@ -2,12 +2,11 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+
-	+:+      */
+/*                                                    +:+ +:+         +:+     */
 /*   By: mazeghou <mazeghou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/23 14:23:19 by mazeghou          #+#    #+#             */
-/*   Updated: 2025/01/24 21:12:37 by mazeghou         ###   ########.fr       */
+/*   Created: 2025/01/26 19:53:27 by mazeghou          #+#    #+#             */
+/*   Updated: 2025/01/26 19:53:28 by mazeghou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +20,13 @@ long long	get_current_time(void)
 	return (tv.tv_sec * 1000LL + tv.tv_usec / 1000);
 }
 
+void	log_state(t_philosopher *philosopher, const char *state)
+{
+	pthread_mutex_lock(&philosopher->mutex_print);
+	printf("%lld %d %s\n", get_current_time(), philosopher->id + 1, state);
+	pthread_mutex_unlock(&philosopher->mutex_print);
+}
+
 void	*thread_routine(void *data)
 {
 	t_philosopher	*philosopher;
@@ -30,41 +36,37 @@ void	*thread_routine(void *data)
 	philosopher = (t_philosopher *)data;
 	left_fork = philosopher->id;
 	right_fork = (philosopher->id + 1) % philosopher->nb_philosophers;
-	printf("nb_times_each_philosopher_has_eaten: %d\n",
-		philosopher->nb_times_each_philosopher_has_eaten);
-	printf("nb_times_each_philosopher_must_eat: %d\n",
-		philosopher->nb_times_each_philosopher_must_eat);
 	while (philosopher->nb_times_each_philosopher_has_eaten < philosopher->nb_times_each_philosopher_must_eat)
 	{
-		pthread_mutex_lock(&philosopher->mutex_print);
-		printf("Philosopher [%d]: is thinking.\n", philosopher->id);
-		pthread_mutex_unlock(&philosopher->mutex_print);
-		usleep(2000);
+		log_state(philosopher, "is thinking");
 		if (philosopher->id % 2 == 0)
 		{
 			pthread_mutex_lock(&philosopher->forks[left_fork]);
+			log_state(philosopher, "has taken a fork");
 			pthread_mutex_lock(&philosopher->forks[right_fork]);
+			log_state(philosopher, "has taken a fork");
 		}
 		else
 		{
 			pthread_mutex_lock(&philosopher->forks[right_fork]);
+			log_state(philosopher, "has taken a fork");
 			pthread_mutex_lock(&philosopher->forks[left_fork]);
+			log_state(philosopher, "has taken a fork");
 		}
-		pthread_mutex_lock(&philosopher->mutex_print);
-		printf("Philosopher [%d]: is eating.\n", philosopher->id);
-		pthread_mutex_unlock(&philosopher->mutex_print);
+		log_state(philosopher, "is eating");
 		philosopher->last_meal = get_current_time();
-		usleep(2000);
+		philosopher->nb_times_each_philosopher_has_eaten++;
+		usleep(philosopher->time_to_eat * 1000);
 		pthread_mutex_unlock(&philosopher->forks[left_fork]);
 		pthread_mutex_unlock(&philosopher->forks[right_fork]);
-		philosopher->nb_times_each_philosopher_has_eaten++;
-		pthread_mutex_lock(&philosopher->mutex_print);
-		printf("Philosopher [%d]: finished eating.\n", philosopher->id);
-		pthread_mutex_unlock(&philosopher->mutex_print);
-		pthread_mutex_lock(&philosopher->mutex_print);
-		printf("Philosopher [%d]: is sleeping.\n", philosopher->id);
-		pthread_mutex_unlock(&philosopher->mutex_print);
-		usleep(1500);
+		log_state(philosopher, "is sleeping");
+		usleep(philosopher->time_to_sleep * 1000);
+		if (get_current_time()
+			- philosopher->last_meal > philosopher->time_to_die)
+		{
+			log_state(philosopher, "died");
+			break ;
+		}
 	}
 	return (NULL);
 }
@@ -80,7 +82,6 @@ int	init_philosophers(char **argv)
 	nb_philosophers = ft_atoi(argv[1]);
 	if (nb_philosophers <= 0)
 		return (ft_error("Invalid number of philosophers."));
-	printf("nb_philosophers: %d\n", nb_philosophers);
 	philosophers = malloc(sizeof(t_philosopher) * nb_philosophers);
 	threads = malloc(sizeof(pthread_t) * nb_philosophers);
 	forks = malloc(sizeof(pthread_mutex_t) * nb_philosophers);
@@ -94,10 +95,14 @@ int	init_philosophers(char **argv)
 		philosophers[i].nb_philosophers = nb_philosophers;
 		philosophers[i].forks = forks;
 		pthread_mutex_init(&philosophers[i].mutex_print, NULL);
-		philosophers[i].nb_times_each_philosopher_must_eat = ft_atoi(argv[5]);
+		philosophers[i].time_to_die = ft_atoi(argv[2]);
+		philosophers[i].time_to_eat = ft_atoi(argv[3]);
+		philosophers[i].time_to_sleep = ft_atoi(argv[4]);
+		philosophers[i].last_meal = get_current_time();
+		philosophers[i].nb_times_each_philosopher_must_eat = argv[5] ? ft_atoi(argv[5]) :
+			-1;
 		philosophers[i].nb_times_each_philosopher_has_eaten = 0;
 	}
-	printf("Philosophers initialized successfully.\n");
 	for (i = 0; i < nb_philosophers; i++)
 		pthread_create(&threads[i], NULL, thread_routine, &philosophers[i]);
 	for (i = 0; i < nb_philosophers; i++)
